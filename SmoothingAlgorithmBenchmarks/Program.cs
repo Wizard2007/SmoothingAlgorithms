5315a2a705f58e1412295a51d2e9a19aa6d90480
Eujine <sponsor85@mail.ru>
2020-02-16 00:38:12 +0200

    [+] Benchmark for Sse2 support class with intrinsic counters

--- SmoothingAlgorithmBenchmarks/Program.cs
+++ SmoothingAlgorithmBenchmarks/Program.cs
@@ -11,23 +11,49 @@ namespace SmoothingAlgorithmBenchmarks
         static void Main(string[] args)
         {
             var configApplay =  CommonApplayConfig.Create(DefaultConfig.Instance);
-            BenchmarkRunner.Run<LoadStoreIntinsicBenchmark>();
+            //BenchmarkRunner.Run<LoadStoreIntinsicBenchmark>();
 
-            /* BenchmarkRunner.Run(new[]{
+            BenchmarkRunner.Run(new[]{
+                #region Simple implementation
+
                 BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowBenchmark), configApplay),
                 BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnrolledBenchmark), configApplay),
+
+                #endregion
+
+                #region Unsafe code
+
                 BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnsafeBenchmark), configApplay),
                 BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnrolledUnsafeBenchmark), configApplay),
+
+                #endregion
+
+                #region Sse2
+
                 BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnrolledUnsafeIntrinsicsBenchmark), configApplay),
                 BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnroledUnsafeIntrinsicsUnrolledBenchmark), configApplay),
                 BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnroledUnsafeIntrinsicsUnrolledPPBenchmark), configApplay),
+                BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnroledUnsafeIntrinsicsUnrolledIncSse2Benchmark), configApplay),
+
+                #endregion
+
+                #region Avx
+
+                BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnroledUnsafeIntrinsicsUnrolledAvxBenchmark), configApplay), 
+                
+                #endregion
+
+                #region Avx2
+
                 BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnroledUnsafeIntrinsicsAvx2Benchmark), configApplay),
                 BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnroledUnsafeIntrinsicsUnrolledAvx2Benchmark), configApplay),
                 BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnroledUnsafeIntrinsicsUnrolledIncAvx2Benchmark), configApplay),
-                BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnroledUnsafeIntrinsicsUnrolledIncAvxBenchmark), configApplay),
-                BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnroledUnsafeIntrinsicsUnrolledAvxBenchmark), configApplay),               
+                BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnroledUnsafeIntrinsicsUnrolledIncAvxBenchmark), configApplay),             
                 BenchmarkConverter.TypeToBenchmarks(typeof(MovingWindowUnroledUnsafeIntrinsicsUnrolledPPAvx2Benchmark), configApplay),
+
+                #endregion
+                
             });
-         */}
+        }
     }
 }
